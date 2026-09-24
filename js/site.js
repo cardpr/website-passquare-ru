692b805d01a87d86a896e2676d0872c455aa7bfa
eagle <code@proorlov.com>
2026-09-24 15:23:33 +0300

    - chat

--- js/site.js
+++ js/site.js
@@ -36,13 +36,6 @@
     }
   }
 
-  document.querySelectorAll('a[href="#sales"]').forEach(function (link) {
-    link.addEventListener("click", function (event) {
-      event.preventDefault();
-      if (window.$chatwoot) window.$chatwoot.toggle();
-    });
-  });
-
   var PREFIX = "cardpr_cookie_";
   var UTMS = ["utm_source", "utm_medium", "utm_campaign", "utm_content", "utm_term"];
   var params = new URLSearchParams(window.location.search);
@@ -84,21 +77,6 @@
       } catch (err) {}
     });
   }
-
-  window.chatwootSettings = { locale: "ru" };
-  (function (d, t) {
-    var g = d.createElement(t);
-    var s = d.getElementsByTagName(t)[0];
-    g.src = "https://app.chatwoot.com/packs/js/sdk.js";
-    g.async = true;
-    s.parentNode.insertBefore(g, s);
-    g.onload = function () {
-      window.chatwootSDK.run({
-        websiteToken: "7TJ8jcxZEZBhebCwkAMz3d2r",
-        baseUrl: "https://app.chatwoot.com"
-      });
-    };
-  })(document, "script");
 
   setTimeout(function () {
     (function (m, e, t, r, i, k, a) {
